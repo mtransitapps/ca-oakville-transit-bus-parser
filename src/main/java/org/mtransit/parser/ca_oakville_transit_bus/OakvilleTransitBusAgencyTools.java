888ce289716a8875f5bb8aabb964d6fe3d7148f4
Mathieu Méa <mathieu.mea@gmail.com>
2019-12-22 10:05:33 -0500

    Update git submodule

--- src/main/java/org/mtransit/parser/ca_oakville_transit_bus/OakvilleTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_oakville_transit_bus/OakvilleTransitBusAgencyTools.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mtransit.parser.CleanUtils;
+import org.mtransit.parser.ColorUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.Pair;
 import org.mtransit.parser.SplitUtils;
@@ -173,7 +174,7 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public String getRouteColor(GRoute gRoute) {
 		String routeColor = gRoute.getRouteColor();
-		if (WHITE.equalsIgnoreCase(routeColor)) {
+		if (ColorUtils.WHITE.equalsIgnoreCase(routeColor)) {
 			routeColor = null; // can't be white
 		}
 		if (StringUtils.isEmpty(routeColor)) {
