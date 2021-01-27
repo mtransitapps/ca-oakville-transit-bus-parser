e7860cb8eda19a8022e84b89096965521713ff76
Mathieu Méa <mathieu.mea@gmail.com>
2021-01-27 11:06:03 -0500

    Cleanup

--- src/main/java/org/mtransit/parser/ca_oakville_transit_bus/OakvilleTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_oakville_transit_bus/OakvilleTransitBusAgencyTools.java
@@ -1,22 +1,12 @@
 package org.mtransit.parser.ca_oakville_transit_bus;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.ColorUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
+import org.mtransit.parser.StringUtils;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -24,18 +14,25 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.mtransit.parser.StringUtils.EMPTY;
 
 // https://portal-exploreoakville.opendata.arcgis.com/
 // https://portal-exploreoakville.opendata.arcgis.com/datasets/oakville-transit-route-and-schedule-information
 // https://www.arcgis.com/sharing/rest/content/items/d78a1c1ad6a940009de8b68839a8f606/data
 public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(String[] args) {
+	public static void main(@Nullable String[] args) {
 		if (args == null || args.length == 0) {
 			args = new String[3];
 			args[0] = "input/gtfs.zip";
@@ -45,58 +42,62 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 		new OakvilleTransitBusAgencyTools().start(args);
 	}
 
-	private HashSet<String> serviceIds;
+	@Nullable
+	private HashSet<Integer> serviceIdInts;
 
 	@Override
-	public void start(String[] args) {
+	public void start(@NotNull String[] args) {
 		MTLog.log("Generating Oakville Transit bus data...");
 		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this, true);
+		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
 		super.start(args);
 		MTLog.log("Generating Oakville Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
 	@Override
 	public boolean excludingAll() {
-		return this.serviceIds != null && this.serviceIds.isEmpty();
+		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
 	}
 
 	@Override
-	public boolean excludeCalendar(GCalendar gCalendar) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendar(gCalendar, this.serviceIds);
+	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
+		if (this.serviceIdInts != null) {
+			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
 		}
 		return super.excludeCalendar(gCalendar);
 	}
 
 	@Override
-	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
+		if (this.serviceIdInts != null) {
+			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
 		}
 		return super.excludeCalendarDate(gCalendarDates);
 	}
 
 	@Override
-	public boolean excludeTrip(GTrip gTrip) {
-		if (this.serviceIds != null) {
-			return excludeUselessTrip(gTrip, this.serviceIds);
+	public boolean excludeTrip(@NotNull GTrip gTrip) {
+		if (this.serviceIdInts != null) {
+			return excludeUselessTripInt(gTrip, this.serviceIdInts);
 		}
 		return super.excludeTrip(gTrip);
 	}
 
+	@NotNull
 	@Override
 	public Integer getAgencyRouteType() {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
+	@NotNull
 	@Override
-	public String getRouteLongName(GRoute gRoute) {
-		return CleanUtils.cleanLabel(gRoute.getRouteLongName().toLowerCase(Locale.ENGLISH));
+	public String getRouteLongName(@NotNull GRoute gRoute) {
+		return CleanUtils.cleanLabel(gRoute.getRouteLongNameOrDefault().toLowerCase(Locale.ENGLISH));
 	}
 
 	private static final String AGENCY_COLOR = "DCA122"; // GOLD (AGENCY LOGO SVG WIKIPEDIA)
 
+	@NotNull
 	@Override
 	public String getAgencyColor() {
 		return AGENCY_COLOR;
@@ -112,14 +113,16 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 	private static final long RID_ENDS_WITH_W = 23_000L;
 
 	@Override
-	public long getRouteId(GRoute gRoute) {
-		if (Utils.isDigitsOnly(gRoute.getRouteId())) {
-			return Long.parseLong(gRoute.getRouteId());
+	public long getRouteId(@NotNull GRoute gRoute) {
+		//noinspection deprecation
+		final String routeId = gRoute.getRouteId();
+		if (Utils.isDigitsOnly(routeId)) {
+			return Long.parseLong(routeId);
 		}
-		Matcher matcher = DIGITS.matcher(gRoute.getRouteId());
+		Matcher matcher = DIGITS.matcher(routeId);
 		if (matcher.find()) {
 			long digits = Long.parseLong(matcher.group());
-			String routeIdLC = gRoute.getRouteId().toLowerCase(Locale.ENGLISH);
+			String routeIdLC = routeId.toLowerCase(Locale.ENGLISH);
 			if (routeIdLC.endsWith("a")) {
 				return RID_ENDS_WITH_A + digits;
 			} else if (routeIdLC.endsWith("b")) {
@@ -137,36 +140,14 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 		throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute);
 	}
 
-	private static final String COLOR_DE242C = "DE242C";
-	private static final String COLOR_7F1C7D = "7F1C7D";
-	private static final String COLOR_4D368A = "4D368A";
-	private static final String COLOR_8A5032 = "8A5032";
-	private static final String COLOR_AD4D45 = "AD4D45";
-	private static final String COLOR_F05B72 = "F05B72";
-	private static final String COLOR_1C4E9D = "1C4E9D";
-	private static final String COLOR_201B18 = "201B18";
-	private static final String COLOR_2EA983 = "2EA983";
-	private static final String COLOR_00A7D8 = "00A7D8";
-	private static final String COLOR_EE3429 = "EE3429";
-	private static final String COLOR_1B6A4D = "1B6A4D";
-	private static final String COLOR_A3238E = "A3238E";
-	private static final String COLOR_00B5DA = "00B5DA";
-	private static final String COLOR_8CBA40 = "8CBA40";
-	private static final String COLOR_333300 = "333300";
-	private static final String COLOR_B479A6 = "B479A6";
-	private static final String COLOR_CA7A2F = "CA7A2F";
-	private static final String COLOR_9DC73E = "9DC73E";
-	private static final String COLOR_8F2E68 = "8F2E68";
-	private static final String COLOR_221E1F = "221E1F";
-	private static final String COLOR_DF241A = "DF241A";
-	private static final String COLOR_DB214C = "DB214C";
-
 	private static final String COLOR_SCHOOL_SPECIALS = "00529B"; // blue (not official)
 
 	private static final String COLOR_SENIOR_SPECIALS = "5B6162"; // dark grey (not official)
 
+	@SuppressWarnings("DuplicateBranchesInSwitch")
+	@Nullable
 	@Override
-	public String getRouteColor(GRoute gRoute) {
+	public String getRouteColor(@NotNull GRoute gRoute) {
 		String routeColor = gRoute.getRouteColor();
 		if (ColorUtils.WHITE.equalsIgnoreCase(routeColor)) {
 			routeColor = null; // can't be white
@@ -175,32 +156,32 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 			int routeId = (int) getRouteId(gRoute);
 			switch (routeId) {
 			// @formatter:off
-			case 1: return COLOR_DE242C;
-			case 2: return COLOR_7F1C7D;
-			case 3: return COLOR_4D368A;
-			case 4: return COLOR_8A5032;
-			case 5: return COLOR_AD4D45;
-			case 5 + (int) RID_ENDS_WITH_A: return COLOR_AD4D45; // 5A
-			case 6: return COLOR_F05B72;
-			case 10: return COLOR_1C4E9D;
-			case 11: return COLOR_201B18;
+			case 1: return "DE242C";
+			case 2: return "7F1C7D";
+			case 3: return "4D368A";
+			case 4: return "8A5032";
+			case 5: return "AD4D45";
+			case 5 + (int) RID_ENDS_WITH_A: return "AD4D45"; // 5A
+			case 6: return "F05B72";
+			case 10: return "1C4E9D";
+			case 11: return "201B18";
 			case 12: return "365981";
-			case 13: return COLOR_2EA983;
-			case 14: return COLOR_00A7D8;
-			case 14 + (int) RID_ENDS_WITH_A: return COLOR_00A7D8; // 14A
-			case 15: return COLOR_EE3429;
-			case 17: return COLOR_1B6A4D;
-			case 18: return COLOR_333300;
-			case 19: return COLOR_A3238E;
-			case 20: return COLOR_00B5DA;
-			case 21: return COLOR_333300;
-			case 22: return COLOR_333300;
-			case 24: return COLOR_8CBA40;
-			case 25: return COLOR_333300;
-			case 26: return COLOR_B479A6;
-			case 28: return COLOR_CA7A2F;
-			case 32: return COLOR_9DC73E;
-			case 33: return COLOR_8F2E68;
+			case 13: return "2EA983";
+			case 14: return "00A7D8";
+			case 14 + (int) RID_ENDS_WITH_A: return "00A7D8"; // 14A
+			case 15: return "EE3429";
+			case 17: return "1B6A4D";
+			case 18: return "333300";
+			case 19: return "A3238E";
+			case 20: return "00B5DA";
+			case 21: return "333300";
+			case 22: return "333300";
+			case 24: return "8CBA40";
+			case 25: return "333300";
+			case 26: return "B479A6";
+			case 28: return "CA7A2F";
+			case 32: return "9DC73E";
+			case 33: return "8F2E68";
 			case 34: return "CAAD35";
 			case 54: return null; // TODO
 			case 55: return null; // TODO
@@ -221,10 +202,10 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 			case 90: return COLOR_SENIOR_SPECIALS;
 			case 91: return COLOR_SENIOR_SPECIALS;
 			case 92: return COLOR_SENIOR_SPECIALS;
-			case 102: return COLOR_221E1F;
-			case 120: return COLOR_DF241A;
+			case 102: return "221E1F";
+			case 120: return "DF241A";
 			case 121: return null; // TODO
-			case 190: return COLOR_DB214C;
+			case 190: return "DB214C";
 			// @formatter:on
 			default:
 				throw new MTLog.Fatal("Unexpected route color for %s!", gRoute);
@@ -233,41 +214,8 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 		return routeColor;
 	}
 
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
 	@Override
-	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@Override
-	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
-	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
+	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
 		if (mRoute.getId() == 81L + RID_ENDS_WITH_N) { // 81N
 			if (gTrip.getDirectionIdOrDefault() == 1) { // FIXES 2 directions with same ID
 				if ("Bronte and Richview".equals(gTrip.getTripHeadsign())) {
@@ -287,21 +235,21 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
+	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
 		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
 		if (mTrip.getRouteId() == 4L) {
 			if (Arrays.asList( //
-					"WEST - Oakville GO", //
-					"WEST - Bronte GO" //
+					"W - Oakville GO", //
+					"W - Bronte GO" //
 			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("WEST - Bronte GO", mTrip.getHeadsignId());
+				mTrip.setHeadsignString("W - Bronte GO", mTrip.getHeadsignId());
 				return true;
 			}
 			if (Arrays.asList( //
-					"EAST - Oakville GO", //
-					"EAST - Clarkson GO" //
+					"E - Oakville GO", //
+					"E - Clarkson GO" //
 			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("EAST - Clarkson GO", mTrip.getHeadsignId());
+				mTrip.setHeadsignString("E - Clarkson GO", mTrip.getHeadsignId());
 				return true;
 			}
 		} else if (mTrip.getRouteId() == 5L) {
@@ -364,48 +312,53 @@ public class OakvilleTransitBusAgencyTools extends DefaultAgencyTools {
 				return true;
 			}
 		}
-		throw new MTLog.Fatal("Unexptected trips to merge %s & %s!", mTrip, mTripToMerge);
+		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 	}
-
-	private static final Pattern GO_ = Pattern.compile("((^|\\W){1}(go)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
-	private static final String GO_REPLACEMENT = "$2" + "GO" + "$4";
 
 	private static final Pattern STARTS_WITH_RSN = Pattern.compile("(^[\\d]+ )", Pattern.CASE_INSENSITIVE);
 
 	private static final Pattern ENDS_WITH_ONLY = Pattern.compile("( only$)", Pattern.CASE_INSENSITIVE);
 
 	private String cleanTripHeadsignWithoutRealTime(String tripHeadsign) {
-		if (Utils.isUppercaseOnly(tripHeadsign, true, true)) {
-			tripHeadsign = tripHeadsign.toLowerCase(Locale.ENGLISH);
-		}
+		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign, getIgnoredWords());
 		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
-		tripHeadsign = STARTS_WITH_RSN.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
-		tripHeadsign = ENDS_WITH_ONLY.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
-		tripHeadsign = GO_.matcher(tripHeadsign).replaceAll(GO_REPLACEMENT);
+		tripHeadsign = STARTS_WITH_RSN.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = ENDS_WITH_ONLY.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
 		tripHeadsign = CleanUtils.cleanSlashes(tripHeadsign);
+		tripHeadsign = CleanUtils.cleanBounds(tripHeadsign);
 		tripHeadsign = CleanUtils.cleanStreetTypes(tripHeadsign);
 		tripHeadsign = CleanUtils.cleanNumbers(tripHeadsign);
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
 	// trip head signs used for real-time API
+	@NotNull
 	@Override
-	public String cleanTripHeadsign(String tripHeadsign) {
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
+	private String[] getIgnoredWords() {
+		return new String[]{
+				"GO", "OTMH", "YMCA",
+		};
+	}
+
+	@NotNull
 	@Override
-	public String cleanStopName(String gStopName) {
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName, getIgnoredWords());
 		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		gStopName = CleanUtils.CLEAN_AND.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
+		gStopName = CleanUtils.cleanBounds(gStopName);
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		gStopName = CleanUtils.cleanNumbers(gStopName);
 		return CleanUtils.cleanLabel(gStopName);
 	}
 
 	@Override
-	public int getStopId(GStop gStop) {
+	public int getStopId(@NotNull GStop gStop) {
 		return Integer.parseInt(gStop.getStopCode()); // use stop code as stop ID
 	}
 }
